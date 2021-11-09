7372cfe63fda51334c4a8e72f8486c81cd812392
eclipse <eclipse@pop-os.localdomain>
2021-11-09 10:18:24 +0100

    modified create channel

--- states/createChannel.js
+++ states/createChannel.js
@@ -5,12 +5,21 @@ const createChannel = require('../utils/createChannel')
 const wordParser = require('../utils/wordParser')
 const command = 'channel'
 
+const admin = [
+    "aaoeclipse",
+    "a2oeclipse"
+]
+
 module.exports = {
     name: command,
     description: 'Create Channel',
     howto: `!${command} [name of book] [chapters]`,
     step: [
        async (mssg, username, client) => {
+        if (!username in admin){
+            client.channel.send("Sorry this is an admin only command");
+            return
+        }
         const user_msg =  wordParser(mssg);
         createChannel(client, user_msg[0], user_msg[1])
        }
